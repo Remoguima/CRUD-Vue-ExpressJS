3b0cfac2c1759f106877e75ae1471149dfe9a128
Remoras <55773988+Remoguima@users.noreply.github.com>
2021-04-05 20:32:56 -0300

    Create Query.sql

---
+++ Query.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE node_crud;
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `diagnosis` varchar(150) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+ 
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+ 
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
